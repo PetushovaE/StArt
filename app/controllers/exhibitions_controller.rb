9f8549ae1f1d7de2069310be49577c6eb1403977
Ekaterina Petushova <epetushova@gmail.com>
2017-01-03 03:05:33 -0500

    started setting users and exhibitions views

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,7 +1,5 @@
 class ExhibitionsController < ApplicationController
 
-
-
 	def index
 		@exhibitions = Exhibition.all
 	end
@@ -10,12 +8,17 @@ class ExhibitionsController < ApplicationController
 		@exhibition = Exhibition.find_by(id: params[:id])
 	end
 
+	def new
+	end
+
 	def create
 	end
 
 	def destroy
 		@exhibition = Exhibition.find_by(id: params[:id])
 		@exhibition.destroy
+		flash[:notice] = "Exhibition deleted."
+    	# redirect_to exhibitions_path
 		redirect_to user_rsvp_path(current_user, @exhibition.rsvp)
 		# end
 	end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,53 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@users = User.all
 	end
 
 	def show
 		@user = User.find_by(id: params[:id])
+	end
+
+	def new
+    	@user = User.new
+  	end
+
+  	def create
+    	@user = User.new(user_params)
+    	respond_to do |format|
+      		if @user.save
+        		session[:user_id] = @user.id
+        		format.html { redirect_to user_path(@user), notice: "Welcome to the StArt!" }
+      		else
+        		format.html { render :new }
+      		end
+    	end
+  	end
+
+  	def edit
+  	end
+
+  	def update
+    	respond_to do |format|
+      		if @user.update(user_params)
+        		findormat.html { redirect_to @user, notice: 'User was successfully updated.' }
+      		else
+        		format.html { render :edit }
+     	 	end
+    	end
+  	end
+
+
+	private
+
+	# Use callbacks to share common setup or constraints between actions.
+	def set_user
+		@user = User.find_by(id: params[:id])
+	end
+
+	def users_params
+		params.require(:user).permit(:name, :password)
 	end
 
 
